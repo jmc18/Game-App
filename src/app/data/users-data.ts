16c1caeb0ae52fba086f198b3e8f7bb391f36614
Jisela Herrera <jisela.herrera.p@gmail.com>
2023-05-29 21:21:51 -0600

    removed user

--- src/app/data/users-data.ts
+++ src/app/data/users-data.ts
@@ -3,5 +3,5 @@ import { User } from "../models/user";
 
 export let userDataLis : User[] = [
         new User(uuid4(), 'jisela.herrera.p@gmail.com', 'Jisela Herrera', '1234567890'),
-        new User(uuid4(), 'mtzcornejo66@gmail.com', 'Javier Martinez', '1234567890'),
+        
 ]
